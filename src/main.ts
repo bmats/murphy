a99b20fa9fbffcf50ca97b99116e3b416c8baf04
Bryce <bmats@users.noreply.github.com>
2015-12-23 17:28:47 -0800

    Add app menu.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {app, BrowserWindow, ipcMain} from 'electron';
+import {app, BrowserWindow, ipcMain, Menu} from 'electron';
 import * as winston from 'winston';
 import BackupConnector from './BackupConnector';
 
@@ -11,20 +11,16 @@ let mainWindow;
 let connector;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+app.on('window-all-closed', () => app.quit());
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', () => {
   // Create the browser window
-  mainWindow = new BrowserWindow({ width: 800, height: 600, minWidth: 660, minHeight: 495, title: 'Murphy' });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, minWidth: 660, minHeight: 495, title: app.getName() });
   winston.info('Created BrowserWindow');
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(require('./menu')));
 
   connector = new BackupConnector(ipcMain, mainWindow.webContents);
 
